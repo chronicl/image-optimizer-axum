e89b450d61eeef6f1424f2c1e3d78878ace714e8
chronicl <lennard.koehler@gmx.net>
2024-01-08 17:27:45 +0100

    axum 0.7

--- examples/basic.rs
+++ examples/basic.rs
@@ -18,9 +18,8 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         let router = axum::Router::new().nest("/images", optimizer.axum_router());
 
         let addr = SocketAddr::from(([127, 0, 0, 1], 3003));
-        axum::Server::bind(&addr)
-            .serve(router.into_make_service())
-            .await?;
+        let listener = tokio::net::TcpListener::bind(&addr).await?;
+        axum::serve(listener, router.into_make_service()).await?;
     }
 
     #[cfg(all(feature = "poem", not(feature = "axum")))]
